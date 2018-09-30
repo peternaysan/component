41d104b4d0770e56d08cde227d58d621787390a0
Imran <imran.nasir@gac.com>
2018-09-30 11:33:42 +0400

    New pages and styles.

---
+++ src/app/commodity/commodity.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'commodity-form',
+    templateUrl: 'commodity.component.html',
+    styleUrls: ['commodity.component.scss']
+})
+
+export class CommodityComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
---
+++ src/app/parties/parties.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'parties-form',
+    templateUrl: 'parties.component.html',
+    styleUrls: ['parties.component.scss']
+})
+
+export class PartiesComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
