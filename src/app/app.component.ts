838de41afc04e43cff9a3bd317c0f8cd49f3f69b
Subindev Vasudevan <subindev.vasudevan@gac.com>
2018-10-09 12:12:17 +0400

    read router param

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { log } from 'util';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'aes-component';
+  constructor(route: ActivatedRoute) {
+    route.queryParams.subscribe(param => {
+      console.log(param.id);
+      // load aes object and make cache it in service so it can be accessed from all components
+    });
+  }
 }
