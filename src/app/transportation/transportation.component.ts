fd9ac6601c657cebcbc16401b3667290ab0172bb
Jyothsna Shriyan <jyothsna.shriyan@gac.com>
2018-10-16 17:03:57 +0400

    changes

--- src/app/transportation/transportation.component.ts
+++ src/app/transportation/transportation.component.ts
@@ -19,7 +19,6 @@ export class TransportationComponent implements OnInit {
        this.route.queryParams.subscribe(param => {
            if(param && param.id){
              this.aesService.getAesById(param.id).then(res=>{
-               console.log(res);
                this.aes=res;
              })
            }          
