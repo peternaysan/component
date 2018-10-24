49ef2b6189e446ee3a6afc5623d0a6bb07aba667
Subindev Vasudevan <subindev.vasudevan@gac.com>
2018-10-24 16:26:09 +0400

    url

--- src/app/services/aes.service.ts
+++ src/app/services/aes.service.ts
@@ -1,4 +1,3 @@
-import { environment } from './../../environments/environment.prod';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl : "https://aesapi.azurewebsites.net/api/"
+  apiUrl : "https://localhost:44396/api/"
 };
 
 /*
