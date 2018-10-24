1175c73126a3ba2a63352605a8c4b470dd900f12
Subindev Vasudevan <subindev.vasudevan@gac.com>
2018-10-24 13:02:31 +0400

    dfg

--- src/app/commodity/commodity.component.ts
+++ src/app/commodity/commodity.component.ts
@@ -3,26 +3,26 @@ import { ActivatedRoute } from '@angular/router';
 import { AesService } from '../services/aes.service';
 
 @Component({
-    selector: 'commodity-form',
-    templateUrl: 'commodity.component.html',
-    styleUrls: ['commodity.component.scss']
+  selector: 'commodity-form',
+  templateUrl: 'commodity.component.html',
+  styleUrls: ['commodity.component.scss']
 })
 
 export class CommodityComponent implements OnInit {
-  public commodityDetails:any =[];
-    constructor(private route: ActivatedRoute, private aesService: AesService) {
-       
-    }
+  public commodityDetails: any = [];
+  constructor(private route: ActivatedRoute, private aesService: AesService) {
 
-   ngOnInit() { 
-       this.route.queryParams.subscribe(param => {
-           if(param && param.id){
-             this.aesService.getAesById(param.id).then(res=>{
-              var aes=res;
-              this.commodityDetails=aes.commodityDetails;
-             })
-           }          
-           // load aes object and make cache it in service so it can be accessed from all components
-         });
-   }
+  }
+
+  ngOnInit() {
+    this.route.queryParams.subscribe(param => {
+      if (param && param.id) {
+        this.aesService.getAesById(param.id).then(res => {
+          var aes = res;
+          this.commodityDetails = aes.commodityDetails;
+        })
+      }
+      // load aes object and make cache it in service so it can be accessed from all components
+    });
+  }
 }
