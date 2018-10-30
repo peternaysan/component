0cbcf9141172d7fa9e8a5b824098450f59a4eb28
Subindev Vasudevan <subindev.vasudevan@gac.com>
2018-10-30 12:25:29 +0400

    loader component

---
+++ src/app/shared/aes-loader/aes-loader.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'aes-loader',
+    templateUrl: 'aes-loader.component.html',
+    styleUrls:['./aes-loader.component.scss']
+})
+
+export class AesLoaderComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
