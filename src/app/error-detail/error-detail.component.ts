f9ad311002fd89116a0e33e34551e241589062e2
Jyothsna Shriyan <jyothsna.shriyan@gac.com>
2018-11-26 08:52:32 +0400

    added customs response table

--- src/app/error-detail/error-detail.component.ts
+++ src/app/error-detail/error-detail.component.ts
@@ -10,9 +10,16 @@ export class ErrorDetailComponent implements OnInit {
     @Output() closeClick = new EventEmitter();
     @Input() aes;
     errorList:any;
+    customsResponseList:any;
+    headerText:string;
     ngOnInit(): void {
         if(this.aes.submissionStatus=="GETS REJECTED"){
+            this.headerText="Error details";
             this.errorList=this.aes.getsResponse.ack.aes.error;
+            
+        }else if(this.aes.submissionStatus=="CUSTOMS REJECTED"){
+            this.headerText="Customs Response details";
+            this.customsResponseList=this.aes.submissionResponse.customsResponseList;
         }
     }
     close() {
