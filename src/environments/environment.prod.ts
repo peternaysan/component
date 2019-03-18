d19b1b7a106d8bc2c063efa6392c541dfa200e5a
Jyothsna Shriyan <jyothsna.shriyan@gac.com>
2019-03-18 08:13:01 +0400

    changed settings

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,14 @@
+// export const environment = {
+//   production: true,
+//   apiUrl : "https://aesapi.azurewebsites.net/api/",
+//   apiBase :"https://aesapi.azurewebsites.net/"
+// };
+
+
 export const environment = {
   production: true,
-  apiUrl : "https://aesapi.azurewebsites.net/api/",
-  apiBase :"https://aesapi.azurewebsites.net/"
+  apiUrl : "https://aesapi.gac.com/api/",
+  apiBase :"https://aesapi.gac.com/"
 };
+
+
