5d16daf2b6174bf0b87ce20ec611602605e680b7
subindev <subindev@outlook.com>
2019-03-24 16:33:55 +0400

    change request - load USSPPI from GF checkbox

--- src/app/parties/parties.component.ts
+++ src/app/parties/parties.component.ts
@@ -12,14 +12,16 @@ import { NgForm } from '@angular/forms';
 })
 export class PartiesComponent implements OnInit {
 
-  @Input() shipmentParty;  
+  @Input() shipmentParty;
   public states: any = [];
   public partyIdType: any = [];
-  public usppiPartyIdTypeList: any=[];
+  public usppiPartyIdTypeList: any = [];
   public partyType: any = [];
   public ultimateConsigneeType: any = [];
   public countryList: any = [];
-  public ultimateConsignee: any;
+  public ultimateConsignee: any = {};
+  public ultimateConsigneeFlag: boolean;
+  public originalUltimateConsignee: any;
   public intermediateConsignee: any;
   public freightForwarder: any;
   public usppi: any;
@@ -28,18 +30,26 @@ export class PartiesComponent implements OnInit {
   }
 
   ngOnInit() {
-    var self = this;
     this.shipmentParty.forEach(element => {
       if (element.partyType == "C") {
-        self.ultimateConsignee = element;
-        self.ultimateConsignee.toBeSoldenRouteIndicator = "N";
+        this.originalUltimateConsignee = JSON.parse(JSON.stringify(element));
+        if (!element.consigneeFromGf) {
+          this.ultimateConsignee = element;
+          this.onChangeConsigneeFromGf(false);
+          this.ultimateConsignee.consigneeFromGf = "N";
+        } else if (element.consigneeFromGf == "Y") {
+          this.ultimateConsigneeFlag = true;
+          this.ultimateConsignee = element;
+        } else {
+          this.ultimateConsignee = element;
+        }
       } else if (element.partyType == "I") {
-        self.intermediateConsignee = element;
+        this.intermediateConsignee = element;
       } else if (element.partyType == "F") {
-        self.freightForwarder = element;
+        this.freightForwarder = element;
       } else if (element.partyType == "E") {
-        self.usppi = element;
-      }       
+        this.usppi = element;
+      }
     });
 
     if (!this.usppi) {
@@ -55,12 +65,12 @@ export class PartiesComponent implements OnInit {
     this.partyType = MasterData.partyTypeList;
     this.countryList = MasterData.countryList;
     this.ultimateConsigneeType = MasterData.ultimateConsigneeType;
-    this.usppiPartyIdTypeList= MasterData.usppiPartyIdTypeList;
+    this.usppiPartyIdTypeList = MasterData.usppiPartyIdTypeList;
   }
 
   submitted = false;
   get isValid() {
-    this.submitted = true;       
+    this.submitted = true;
     return this.partiesForm.valid;
   }
   searchByIdAndName(term: string, item: any) {
@@ -77,6 +87,25 @@ export class PartiesComponent implements OnInit {
     else {
       return name.indexOf(term) > -1;
     }
+  }
+
+  onChangeConsigneeFromGf() {
+    if (this.ultimateConsigneeFlag) {
+      for (var k in this.originalUltimateConsignee)
+        this.ultimateConsignee[k] = this.originalUltimateConsignee[k];
+      this.ultimateConsignee.consigneeFromGf = "Y";
+    }
+    else {
+      // for (const prop of Object.keys(this.ultimateConsignee)) {
+      //   delete this.ultimateConsignee[prop];
+      // }
+      for (var k in this.ultimateConsignee)
+        this.ultimateConsignee[k] = null;
+      this.ultimateConsignee.consigneeFromGf = "N";
+      this.ultimateConsignee.partyType = "C";
+    }
+    this.ultimateConsignee.toBeSoldenRouteIndicator = "N";
+
   }
 
 }
