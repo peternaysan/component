865c1849f637c00754105b950dc9a3a229c4b680
Jyothsna Shriyan <jyothsna.shriyan@gac.com>
2019-04-08 10:34:56 +0400

    aes print out issue fix

--- src/app/print-view/print-view.component.ts
+++ src/app/print-view/print-view.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import * as jspdf from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
     selector: 'aes-print-view',
@@ -9,18 +11,41 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 export class AesPrintViewComponent implements OnInit {
     @Input() aes;
     @Output() closeClick = new EventEmitter();
-    public ultimateConsignee: any;
-    public intermediateConsignee: any;
-    public freightForwarder: any;
     constructor() { }
 
     ngOnInit() {
         var self = this;
-     
-     }
+
+    }
 
     print() {
-        window.print()
+        // window.print()
+        var data = document.getElementById('printbox');
+        var images = []
+        
+        html2canvas(data).then(canvas => {
+            // Few necessary setting options                       
+            const contentDataURL = canvas.toDataURL('image/png')
+            var imgWidth = 210; 
+            var pageHeight = 295;  
+            var imgHeight = canvas.height * imgWidth / canvas.width;
+            var heightLeft = imgHeight;
+  
+            var doc = new jspdf('p', 'mm');
+            var position = 0;
+  
+            doc.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight);
+            heightLeft -= pageHeight;
+  
+            while (heightLeft >= 0) {
+              position = heightLeft - imgHeight-5;
+              doc.addPage();
+              doc.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight);
+              heightLeft -= pageHeight;
+            }
+            doc.autoPrint();
+            doc.save(this.aes.shipmentHeader.shipmentReferenceNumber+'.pdf');           
+        });
     }
 
     close() {
