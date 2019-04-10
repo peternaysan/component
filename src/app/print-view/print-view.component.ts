1b3c8dfc9bec945d632807d85a36317d86eeca09
Imran Nasir <imran.nasir@gac.com>
2019-04-10 13:50:56 +0400

    print changes.

--- src/app/print-view/print-view.component.ts
+++ src/app/print-view/print-view.component.ts
@@ -19,30 +19,30 @@ export class AesPrintViewComponent implements OnInit {
     }
 
     print() {
-        // window.print()
+        window.print()
         var data = document.getElementById('printbox');        
-        html2canvas(data).then(canvas => {
-            // Few necessary setting options                       
-            const contentDataURL = canvas.toDataURL('image/png')
-            var imgWidth = 200;
-            var pageHeight = 290;
-            var imgHeight = canvas.height * imgWidth / canvas.width;
-            var heightLeft = imgHeight;
+        // html2canvas(data).then(canvas => {
+        //     // Few necessary setting options                       
+        //     const contentDataURL = canvas.toDataURL('image/png')
+        //     var imgWidth = 200;
+        //     var pageHeight = 290;
+        //     var imgHeight = canvas.height * imgWidth / canvas.width;
+        //     var heightLeft = imgHeight;
 
-            var doc = new jspdf('p', 'mm', 'a4');
-            var position = 6;
-            doc.addImage(contentDataURL, 'JPEG', 6, position, imgWidth, imgHeight);
-            heightLeft -= pageHeight;
+        //     var doc = new jspdf('p', 'mm', 'a4');
+        //     var position = 6;
+        //     doc.addImage(contentDataURL, 'JPEG', 6, position, imgWidth, imgHeight);
+        //     heightLeft -= pageHeight;
 
-            while (heightLeft >= 0) {
-                position = heightLeft - imgHeight;
-                doc.addPage();
-                doc.addImage(contentDataURL, 'PNG', 6, position, imgWidth, imgHeight);
-                heightLeft -= pageHeight;
-            }
-            doc.autoPrint();
-            doc.save(this.aes.shipmentHeader.shipmentReferenceNumber + '.pdf');
-        });
+        //     while (heightLeft >= 0) {
+        //         position = heightLeft - imgHeight;
+        //         doc.addPage();
+        //         doc.addImage(contentDataURL, 'PNG', 6, position, imgWidth, imgHeight);
+        //         heightLeft -= pageHeight;
+        //     }
+        //     doc.autoPrint();
+        //     doc.save(this.aes.shipmentHeader.shipmentReferenceNumber + '.pdf');
+        // });
     }
 
     close() {
